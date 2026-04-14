9705dcbb4cdda4e3db9b5e058254d06385032937
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-14 14:46:16 +0300

    Update views.py

--- apps/placements/views.py
+++ apps/placements/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import InternshipPlacement
 from .serializers import InternshipPlacementserializer# Create your views here.
+
+class InternshipPlacementViewSet(viewsets.ModelViewSet):
+    queryset = InternshipPlacement.objects.all()
+    serializer_class = InternshipPlacementserializer
+    permission_classes = [permissions.IsAuthenticated]
