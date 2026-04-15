8cbce68b4bbefe2ea3abf669c76a362a18f2a981
gift-mercy <giftmercy81730@gmail.com>
2026-04-15 10:39:17 +0300

    changed serializer

--- apps/evaluations/serializers.py
+++ apps/evaluations/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Evaluation
+
+class EvaluationSerializer(serializers.ModelSerializers):
+    class Meta:
+        model = Evaluation
+        fields = '__all__'
