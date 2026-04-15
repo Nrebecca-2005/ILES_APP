0c3cd06e07ceedc26e6cb7a1b70d7a13e9ba3ca0
gift-mercy <giftmercy81730@gmail.com>
2026-04-15 13:03:28 +0300

    updated urls

--- apps/evaluations/urls.py
+++ apps/evaluations/urls.py
@@ -3,3 +3,8 @@ from rest_frameworks.routers import DefaultRouter
 from .views import EvaluationViewSet
 
 router = DefaultRouter()
+router.register(r'evaluations', EvaluationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
