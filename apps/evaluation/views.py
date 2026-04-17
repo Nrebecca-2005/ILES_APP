6e76f6ddc5a6b8cb358ab6ab991fb597b66d70f0
gift-mercy <giftmercy81730@gmail.com>
2026-04-17 10:27:31 +0300

    updated project

--- apps/evaluation/views.py
+++ apps/evaluation/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Evaluation
 from .forms import EvaluationForm
 
+def creat_evaluation(request):
+    if request.method == "POST"
+        form = EvaluationForm(request.POST)
+        if form. is_valid():
+            form.save()
+            return redirect('evaluation list')
+    else:
+        form = EvaluationForm()
+    return render(request, 'create_evaluation.html',  {'form': form})
+
+        
+
 
 # Create your views here.
